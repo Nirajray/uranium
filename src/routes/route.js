34c7b4d3791f694335d3b33001064a98eb99b06c
Nirajray <nirajray5555@gmail.com>
2022-04-08 00:44:21 +0530

    my params are

--- src/routes/route.js
+++ src/routes/route.js
@@ -7,18 +7,73 @@ router.get('/user-profile/:abcd', function(req, res) {
     console.log(req)
     console.log(req.params.abcd)
     res.send('dummy response')
-})
-
-router.get('/test-me', function (req, res) {
-    console.log('------------------')
-    console.log(req)
-    console.log('------------------')
-    console.log('These are the request query parameters: ', req.query)
-    res.send('My first ever api!')
 });
 
+// Problem 1-----------------------------------------------------------------
+router.get('/movies', function(req, res) {
+    let arr = ['Rang De Basanti', 'Inception', 'King', 'RRR']
+    res.send(arr)
+});
+
+// Problem 2 ---------------------------------------------------------------
+
+router.get('/movie/:index', function (req, res) {
+    let arr = ['Rang De Basanti', 'Inception', 'King', 'RRR'];
+     res.send(arr[req.params.index])
+});
+
+// Problem no 3 -------------------------------------------------------------
+
+router.get('/movies/:index', function (req, res) {
+    let arr = ['Rang De Basanti', 'Inception', 'King', 'RRR'];
+    if (req.params.index>arr.length-1){
+        res.send("the index is exceeded")
+    }
+     res.send(arr[req.params.index])
+});
+
+// Problem 4 -----------------------------------------------------------------
+router.get('/films', function (req, res) {
+    movies = [ {
+        "id": 1,
+        "name": "The Shining"
+       }, {
+        "id": 2,
+        "name": "Incendies"
+       }, {
+        "id": 3,
+        "name": "Rang de Basanti"
+       }, {
+        "id": 4,
+        "name": "Finding Nemo" }];
+        res.send(movies)
+});
+ 
+// Problem no 5 -------------------------------------------------
+
+router.get('/films/:filmId', function (req, res) {
+    
+    movies = [ {
+        "id": 1,
+        "name": "The Shining"
+       }, {
+        "id": 2,
+        "name": "Incendies"
+       }, {
+        "id": 3,
+        "name": "Rang de Basanti"
+       }, {
+        "id": 4,
+        "name": "Finding Nemo" }];
+        if(req.params.filmId > movies.length -1){
+
+          res.send("the filmid no is exceeded")      
+
+        }
+res.send(movies[req.params.filmId])
 
 
+});
 
 module.exports = router;
 // adding this comment for no reason
---
+++ src/validator/formatter.js
@@ -0,0 +1,19 @@
+
+let text = "  function up "
+let trim = function()
+{
+return text.trim()
+}
+
+ let changeToLowerCase = function(){
+return text.toLowerCase()
+}
+
+let changeToUperCase = function(){
+return text.toUpperCase()
+}
+module.exports.trim = trim ;
+module.exports.changeToLowerCase = changeToLowerCase ;
+module.exports.changeToUperCase = changeToUperCase ;
+
+
