80990e505fc4afb1e302e8d87bfdf900bb9708b6
Nirajray <nirajray5555@gmail.com>
2022-04-09 09:42:51 +0530

    my changes

--- src/routes/route.js
+++ src/routes/route.js
@@ -65,15 +65,57 @@ router.get('/films/:filmId', function (req, res) {
        }, {
         "id": 4,
         "name": "Finding Nemo" }];
-        if(req.params.filmId > movies.length -1){
+        if(req.params.filmId > movies.length){
 
           res.send("the filmid no is exceeded")      
 
         }
+        for(let i = 0; i< movies.length;i++){
+            if(req.params.filmId == movies[i].id){
+                res.send(movies[i])}
+            }
 res.send(movies[req.params.filmId])
 
 
 });
+router.get('/array', function (req, res) {
+    function missNo(arr, n ){
+         let nArrSum = (n*(n+1)/2);
+        let sumOfTheArr= 0 ;
+        for(let i = 0; i < arr.length;i++){
+        
+
+            sumOfTheArr += arr[i]; 
+        }
+            return nArrSum-sumOfTheArr  ; 
+    
+    }
+    let arr =[1, 2, 3, 4, 5, 6, 7, 8, 9, 11] ;
+    let n = arr.length + 1;
+    let miss = missNo(arr, n)
+
+console.log(miss)
+     res.send(miss.toString())
+
+});
+
+
+router.get('/rangemiss', function (req, res) {
+    let arr = [32, 33, 34, 35, 37, 38, 39]
+    let w = 0;
+    for(let i=arr[0]; i < arr[arr.length-1]; i++){
+
+        w +=i;
+    }
+
+    let y = 0;
+    for (let j = 0; j< arr.length; j++){
+        y +=j[i];
+    }
+
+    let k = w - y;
+    res.send(k.toString())
+});
 
 module.exports = router;
-// adding this comment for no reason
+// adding this comment for no reason------
